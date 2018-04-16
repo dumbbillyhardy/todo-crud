5004375d3b43b6d457dab8c77f25902a75acaf3d
Billy Hardy <billy92@gmail.com>
2018-04-15 20:22:04 -0400

    change syntax to es5 to sate webpack

--- query-mixin.js
+++ query-mixin.js
@@ -5,11 +5,13 @@ export const QueryMixin = (clazz) => class extends clazz {
     $all(cssSelector) {
         return [...this.shadowRoot.querySelectorAll(cssSelector)];
     }
-    fire(eventName, detail, eventConfig) {
-        this.dispatchEvent(new CustomEvent(eventName, {
-            detail,
-            ...eventConfig
-        }));
+    fire(eventName, detail, eventConfig={}) {
+        const opt = {};
+        opt.detail = detail;
+        for(const key of Object.keys(eventConfig)) {
+            opt[key] = eventConfig[key];
+        }
+        this.dispatchEvent(new CustomEvent(eventName, opt));
     }
     getSlotted(cssSelector) {
         return this.querySelector(cssSelector) || this.$$(cssSelector);
